8a5706390571169360890b1683ba115aaededf71
Aaron Ferrer <aaronferrerbarbas@gmail.com>
2024-10-20 20:42:10 +0200

    done task jasmine friday

--- src/add.js
+++ src/add.js
@@ -1,7 +1,21 @@
-function add (numOne, numTwo) {
+function add(numOne, numTwo) {
     if (numOne == undefined || numTwo == undefined) {
         return undefined;
     }
 
     return numOne + numTwo;
+}
+
+function add(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return undefined;
+    }
+    return a + b;
+}
+
+function divide(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return undefined;
+    }
+    return a / b;
 }
--- tests/add.spec.js
+++ tests/add.spec.js
@@ -1,24 +1,50 @@
 describe("Iteration 1 | Getting Started", () => {
-    describe("Function add", () => {
-      it("should be defined", () => {
-        expect(add).toBeDefined();
+  describe("Function add", () => {
+    it("should be defined", () => {
+      expect(add).toBeDefined();
+    });
+
+    it("should take two arguments", () => {
+      expect(add.length).toBe(2);
+    });
+
+    it("should return the sum of the two numbers", () => {
+      expect(add(1, 2)).toEqual(3);
+      expect(add(3, 4)).toEqual(7);
+      expect(add(100, 47)).toEqual(147);
+    });
+
+    it("should return undefined if any of the arguments is not provided", () => {
+      expect(add(1)).toEqual(undefined);
+      expect(add()).toEqual(undefined);
+      expect(add(undefined, 1)).toEqual(undefined);
+    });
+    // Additional test
+    it("should return undefined if either of the arguments is not a number", function () {
+      expect(add(2, "three")).toBeUndefined();
+      expect(add("two", 3)).toBeUndefined();
+      expect(add("two", "three")).toBeUndefined();
+    });
+    // Second Additional test
+    describe("Divide Function", function () {
+      it("should be defined", function () {
+        expect(divide).toBeDefined();
       });
-  
-      it("should take two arguments", () => {
-        expect(add.length).toBe(2);
+
+      it("should return the division of two numbers", function () {
+        expect(divide(6, 2)).toBe(3);
+        expect(divide(9, 3)).toBe(3);
       });
-  
-      it("should return the sum of the two numbers", () => {
-        expect(add(1, 2)).toEqual(3);
-        expect(add(3, 4)).toEqual(7);
-        expect(add(100, 47)).toEqual(147);
+
+      it("should return undefined if any argument is not a number", function () {
+        expect(divide(6, "two")).toBeUndefined();
+        expect(divide("six", 2)).toBeUndefined();
       });
-  
-      it("should return undefined if any of the arguments is not provided", () => {
-        expect(add(1)).toEqual(undefined);
-        expect(add()).toEqual(undefined);
-        expect(add(undefined, 1)).toEqual(undefined);
+
+      it("should return undefined if arguments are not provided", function () {
+        expect(divide(6)).toBeUndefined();
+        expect(divide()).toBeUndefined();
       });
     });
   });
-  
+});
